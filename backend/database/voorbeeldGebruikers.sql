d640a5528d5c5217ceb501d4118bb5d370cf1555
andresdeklerck2023 <149085225+andresdeklerck2023@users.noreply.github.com>
2024-12-28 15:06:51 +0100

    Delete backend directory

--- backend/database/voorbeeldGebruikers.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Gebruiker (id, voornaam, achternaam, email, niveau, projectId, wachtwoord) VALUES
-(1, 'Alice', 'Jansen', 'alice.jansen@example.com', 0, NULL, 'admin123'),
-(2, 'Bob', 'Pieters', 'bob.pieters@example.com', 1, 1, 'coach456'),
-(3, 'Charlie', 'Verhoeven', 'charlie.verhoeven@example.com', 2, 2, 'student789'),
-(4, 'Diana', 'Mertens', 'diana.mertens@example.com', 2, 3, 'student456'),
-(5, 'Eve', 'De Vries', 'eve.devries@example.com', 1, NULL, 'coach123');
