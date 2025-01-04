fe906d90733923a1c4430d8ad0cebeb42e0d3707
andresdeklerck2023 <149085225+andresdeklerck2023@users.noreply.github.com>
2025-01-05 00:42:12 +0100

    Update page-tools.js

--- frontend/src/page-tools.js
+++ frontend/src/page-tools.js
@@ -1,7 +1,7 @@
 export const apiURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001/api/"
 
 export let CheckUserLS = () => {
-  let userArr//Normaal formaat: ["gebruikers naam", "wachtwoord", id, niveau, pid]
+  let userArr //Normaal formaat: ["gebruikers naam", "wachtwoord", id, niveau, pid]
   try{
     userArr = JSON.parse(localStorage.getItem("user"))
     if(userArr.length != 5){
@@ -14,24 +14,21 @@ export let CheckUserLS = () => {
 }
 
 export let getData = async (url, body=null, method="get") => {
-    try {
-      const response = await fetch(url,{method: method, body: body, headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },})
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      if(response.headers.get("content-type").indexOf("application/json") !== -1){
-        return await response.json();
-      }else{
-        return await response.text();
-      }
-      
-  } catch (error) {
-    console.error(error.message);
-    return null;
-  }
+  try {
+    const response = await fetch(url,{method: method, body: body})
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    if(response.headers.get("content-type").indexOf("application/json") !== -1){
+      return await response.json();
+    }else{
+      return await response.text();
+    }
+    
+} catch (error) {
+  console.error(error.message);
+  return null;
+}
 }
 
 export default [apiURL, CheckUserLS, getData]
